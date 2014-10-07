ceb0b36e405da5b9611cc081b1c440881b53e471
Linus Unnebäck <linus@folkdatorn.se>
2014-10-07 17:21:06 +0200

    export file readers

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ exports.Unit = require('./lib/unit');
 exports.Map = require('./lib/map');
 exports.Terrain = require('./lib/terrain');
 exports.Scenario = require('./lib/scenario');
+
+exports.DAT = require('./lib/dat');
+exports.DRS = require('./lib/drs');
+exports.SLP = require('./lib/slp');
