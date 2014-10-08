dfce7cdc49b4f53def5a6ad6447f8b9537bb9ef5
Linus Unnebäck <linus@folkdatorn.se>
2014-10-08 12:28:28 +0200

    export palette

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 exports.Map = require('./lib/map');
 exports.Scenario = require('./lib/scenario');
 
+exports.Palette = require('./lib/palette');
+
 exports.DAT = require('./lib/dat');
 exports.DRS = require('./lib/drs');
 exports.SLP = require('./lib/slp');
