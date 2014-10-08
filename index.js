22933d14453eb111bb3eeeeb3d75b03e00710e68
Linus Unnebäck <linus@folkdatorn.se>
2014-10-08 12:27:32 +0200

    map: remove terrain class

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 exports.Unit = require('./lib/unit');
 
 exports.Map = require('./lib/map');
-exports.Terrain = require('./lib/terrain');
 exports.Scenario = require('./lib/scenario');
 
 exports.DAT = require('./lib/dat');
